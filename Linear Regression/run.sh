4a4a940d257a881830c98e62c02375b4f3163a9c
vikaskommalapati4600 <83658659+vikaskommalapati4600@users.noreply.github.com>
2024-10-18 23:55:54 -0600

    Create run.sh

---
+++ Linear Regression/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "For the output of batch_gradient_descent"
+python3 batchGradient.py
+
+echo "For the output of stochastic_gradient_descent"
+python3 stochastiGradient.py
+
+echo "For then output of optimalweight"
+python3 optimalGradient.py
