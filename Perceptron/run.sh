679180abf541786a2756449248ad80d460453ea4
vikaskommalapati4600 <83658659+vikaskommalapati4600@users.noreply.github.com>
2024-11-08 15:27:29 -0700

    Create run.sh

---
+++ Perceptron/run.sh
@@ -0,0 +1,8 @@
+echo "For the output of standard perceptron"
+python3 StandardPerceptron.py
+
+echo "For the output of voted perceptron"
+python3 VotedPerceptron.py
+
+echo "For the output of average perceptron"
+python3 AveragePerceptron.py
